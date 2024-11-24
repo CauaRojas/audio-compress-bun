529db7203d41318973da3baca1b80961253eb49e
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2024-11-24 16:45:09 -0300

    finished?

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 // ffmpeg -i "$inputFilePath" -codec:a libmp3lame -qscale:a 2 "$outputFilePath"
-import {$} from "bun";
+import { $ } from "bun";
 const file = Bun.argv[2];
 const level = Bun.argv[3] || "2";
 
@@ -12,8 +12,9 @@ if (parsed < 0 || parsed > 9) {
   throw new Error("Level must be between 0 and 9");
 }
 
+
 const fileName = file.split(".")[0]
 const ext = file.split(".")[1]
 const outputFilePath = `${fileName}_compressed.${ext}`
 
-$`ffmpeg -i "${file}" -codec:a libmp3lame -qscale:a ${level} "${outputFilePath}"`;
+await $`ffmpeg -i "${file}" -codec:a libmp3lame -qscale:a ${level} "${outputFilePath}"`;
