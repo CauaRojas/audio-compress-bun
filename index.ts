eb2f80596c3038aff10b3ed3596ace4331dd6d04
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2024-11-24 16:30:09 -0300

    init

---
+++ index.ts
@@ -0,0 +1,19 @@
+// ffmpeg -i "$inputFilePath" -codec:a libmp3lame -qscale:a 2 "$outputFilePath"
+import {$} from "bun";
+const file = Bun.argv[2];
+const level = Bun.argv[3] || "2";
+
+const parsed = parseInt(level);
+
+if (isNaN(parsed)) {
+  throw new Error("Invalid level");
+}
+if (parsed < 0 || parsed > 9) {
+  throw new Error("Level must be between 0 and 9");
+}
+
+const fileName = file.split(".")[0]
+const ext = file.split(".")[1]
+const outputFilePath = `${fileName}_compressed.${ext}`
+
+$`ffmpeg -i "${file}" -codec:a libmp3lame -qscale:a ${level} "${outputFilePath}"`;
